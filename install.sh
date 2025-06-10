0a0b1758801b887d47646f4f4c5e78e0f8b0ce89
Albin Engqvist <albinengqvist@gmail.com>
2025-06-10 12:42:59 +0000

    add gitconfig

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ ln -sfn "$HOME/dotfiles/.config/helix" "$HOME/.config/helix"
 
 # Lägg till fler configs här:
 # ln -sfn "$HOME/dotfiles/.config/eza" "$HOME/.config/eza"
-
+ln -sf "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 echo "✅ Klar!"
